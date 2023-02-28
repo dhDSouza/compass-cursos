eeb5cb5c18e38c98e1f9336863003fd416ca8084
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-02-27 23:22:17 -0300

    Generate a hash and convert to a 16-digit uint.

--- CryptoZombies/contract.sol
+++ CryptoZombies/contract.sol
@@ -18,7 +18,8 @@ contract ZombieFactory {
     }
 
     function _generateRandomDna(string _str) private view returns (uint){
-        
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;        
     }    
 
 }
