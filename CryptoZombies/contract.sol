3aed94f0987bcb5da18f5d84d704c5a2077f360a
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-02-27 23:29:33 -0300

    Public function that calls the other functions.

--- CryptoZombies/contract.sol
+++ CryptoZombies/contract.sol
@@ -20,6 +20,11 @@ contract ZombieFactory {
     function _generateRandomDna(string _str) private view returns (uint){
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;        
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }    
 
 }
