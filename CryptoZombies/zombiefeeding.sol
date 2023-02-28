97c72312d379f0df1bd1119b6e3f84892f4bc38a
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-02-28 17:13:02 -0300

    Use the 'Kittyinterface'

--- CryptoZombies/zombiefeeding.sol
+++ CryptoZombies/zombiefeeding.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.19;
 import "./zombiefactory.sol";
 
 contract KittyInterface {
+    
     function getKitty(uint256 _id) external view returns (
         bool isGestating,
         bool isReady,
@@ -18,13 +19,15 @@ contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
-
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      Zombie storage myZombie = zombies[_zombieId];
-      _targetDna = _targetDna % dnaModulus;
-      uint newDna = (myZombie.dna + _targetDna) / 2;
-      _createZombie("NoName", newDna);      
-  }    
     
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);    
+    
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);      
+    }
 }
