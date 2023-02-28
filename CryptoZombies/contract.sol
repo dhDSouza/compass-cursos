09dd1667845e7a71642fc1e93f7e3d529c0442b3
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-02-27 23:04:42 -0300

    Changing the role's access to private.

--- CryptoZombies/contract.sol
+++ CryptoZombies/contract.sol
@@ -6,14 +6,14 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie{
+    struct Zombie {
         string name;
         uint dna;
     }   
 
     Zombie[] public zombies; 
 
-    function createZombie(string _name, uint _dna){
+    function _createZombie(string _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
 
