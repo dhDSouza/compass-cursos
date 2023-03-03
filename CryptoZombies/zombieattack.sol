2699cfc2a39b362b45eca966bcda2ad620f86e77
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-03-03 17:12:05 -0300

    Refactor the "feedAndMultiply" function.

--- CryptoZombies/zombieattack.sol
+++ CryptoZombies/zombieattack.sol
@@ -5,10 +5,15 @@ import "./zombiehelper.sol";
 contract ZombieBattle is zombiehelper {
     
     uint randNonce = 0;
+    uint attackVictoryProbability  = 70;
     
     function randMod(uint _modulus) internal returns(uint) {
         randNonce++;
         return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+    }    
+
+    function attack(uint _zombieId, uint _targetId) external {
+        
     }    
 
 }
