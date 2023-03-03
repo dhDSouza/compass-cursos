f28d90aeaeb4236ab399d1e917d8536496fe07d0
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-03-03 16:02:59 -0300

    Create levelUp function

--- CryptoZombies/zombiehelper.sol
+++ CryptoZombies/zombiehelper.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+
+    uint levelUpFee = 0.001 ether;
     
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }    
 
     function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
@@ -29,6 +36,6 @@ contract ZombieHelper is ZombieFeeding {
             }
         }
         return result;
-    }
+    }    
 
 }
