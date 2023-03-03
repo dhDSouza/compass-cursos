802d313e8c0a2ec33b06df135bfa3b3a43df1eaf
Daniel Henrique de Souza <daniel.souza.pb@compasso.com.br>
2023-03-03 17:53:50 -0300

    Increment conditional structure in attack function

--- CryptoZombies/zombieattack.sol
+++ CryptoZombies/zombieattack.sol
@@ -15,7 +15,13 @@ contract ZombieBattle is zombiehelper {
     function attack(uint _zombieId, uint _targetId) external {
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
-        uint rand = randMod(100);        
+        uint rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        }               
     }    
 
 }
